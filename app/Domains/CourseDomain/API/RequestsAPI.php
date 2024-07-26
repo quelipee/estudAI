617daa9fbab19e197bd310a6f0705706f55044f9
felipe <felipemateus112012@gmail.com>
2024-07-26 16:20:54 -0300

    insert history in iaChat

--- app/Domains/CourseDomain/API/RequestsAPI.php
+++ app/Domains/CourseDomain/API/RequestsAPI.php
@@ -4,7 +4,8 @@ namespace App\Domains\CourseDomain\API;
 
 use GeminiAPI\Resources\Parts\TextPart;
 use Psr\Http\Client\ClientExceptionInterface;
-
+use GeminiAPI\Resources\Content;
+use GeminiAPI\Enums\Role;
 class RequestsAPI
 {
     public function __construct(
@@ -16,9 +17,27 @@ class RequestsAPI
      */
     public function requestChat()
     {
-        $response = $this->cliente->geminiPro()->generateContent(
-          new TextPart('me de um cronograma de um curso basico de python'),
-        );
+// Definindo o histórico
+        $history = [
+            Content::text('Bom dia', Role::User),
+            Content::text(
+                <<<TEXT
+                <?php
+                echo "Hello World!";
+                ?>
+
+                This code will print "Bom Dia!" to the standard output.
+                TEXT,
+                Role::Model,
+            ),
+        ];
+
+        $chat = $this->cliente->geminiPro()
+            ->startChat()
+            ->withHistory($history);
+
+        $response = $chat->sendMessage(new TextPart('voce pode me ensinar python?'));
+        print_r($response->text());
 
         return response()->json([
             'message' => 'request to chat ok',
@@ -26,3 +45,12 @@ class RequestsAPI
         ]);
     }
 }
+
+/*$client = new Client('GEMINI_API_KEY');
+$chat = $client->geminiPro()->startChat();
+
+$response = $chat->sendMessage(new TextPart('Hello World in PHP'));
+print $response->text();
+
+$response = $chat->sendMessage(new TextPart('in Go'));
+print $response->text();*/
